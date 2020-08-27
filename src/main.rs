bc3a56260045264d974ab2be63495f5d204029e6
Mendelt Siebenga <msiebenga@gmail.com>
2020-08-27 22:56:17 +0200

    run command on connection

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 use sqlx::postgres::PgConnection;
-use sqlx::Connection;
+use sqlx::{Connection, Executor};
 
 #[async_std::main]
 async fn main() -> Result<(), sqlx::Error> {
-    let conn = PgConnection::connect(
+    let mut conn = PgConnection::connect(
         "postgresql://example-user:password123@localhost:5432/example-database",
     )
-    .await;
+    .await?;
+
+    conn.execute("BEGIN").await?;
+
+    // let row: (i64,) = sqlx::query_as("SELECT $1")
+    //     .bind(150_i64)
+    //     .fetch_one(&conn).await?;
+
     Ok(())
 }
