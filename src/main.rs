3e321b4cc49e78b71c1812d39d3c32a11f136743
Mendelt Siebenga <msiebenga@gmail.com>
2020-08-27 22:51:14 +0200

    set up connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::postgres::PgConnection;
+use sqlx::Connection;
+
+#[async_std::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let conn = PgConnection::connect(
+        "postgresql://example-user:password123@localhost:5432/example-database",
+    )
+    .await;
+    Ok(())
 }
