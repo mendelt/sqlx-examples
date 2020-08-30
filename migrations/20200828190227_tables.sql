167fab0491adb434846d23038b002dfcd25f1a79
Mendelt Siebenga <msiebenga@gmail.com>
2020-08-30 22:35:54 +0200

    expand on migration script

--- migrations/20200828190227_tables.sql
+++ migrations/20200828190227_tables.sql
@@ -1,6 +1,17 @@
+
 CREATE TABLE example_table (
+    id bigint generated always as identity primary key,
+    name varchar not null,
+    value varchar not null
+);
+
+INSERT INTO example_table (name, value) VALUES
+    ('name1', 'some value'),
+    ('name2', 'some other value');
+
+CREATE TABLE json_table (
     id bigint generated always as identity primary key,
     name varchar not null,
 
     json_stuff jsonb not null
-)
+);
