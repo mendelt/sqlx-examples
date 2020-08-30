c7605758280422c562d704230d8c1d7f311ba897
Mendelt Siebenga <msiebenga@gmail.com>
2020-08-30 23:06:51 +0200

    finish map example

--- examples/map.rs
+++ examples/map.rs
@@ -1,6 +1,5 @@
 use sqlx::prelude::*;
 use sqlx::{query_as, PgPool};
-use serde_json::Value;
 
 const DB_URI: &str = "postgresql://example-user:password123@localhost:5432/example-database";
 
@@ -8,18 +7,21 @@ const DB_URI: &str = "postgresql://example-user:password123@localhost:5432/examp
 async fn main() -> Result<(), sqlx::Error> {
     let pool = connect_db(DB_URI).await;
 
-    let stuff = query_as!(Stuff, 
-        "SELECT id, name, json_stuff FROM example_table")
+    let stuff = query_as::<_, Stuff>( 
+        "SELECT id, name, value FROM example_table")
         .fetch_all(&pool)
         .await?;
+
+    println!("{:?}", stuff);
 
     Ok(())
 }
 
+#[derive(Debug, FromRow)]
 struct Stuff {
     id: i64,
     name: String,
-    json_stuff: Value,
+    value: String,
 }
 
 async fn connect_db(uri: &str) -> PgPool {
