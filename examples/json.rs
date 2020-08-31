78596e4ebb009d608a0c007babdb40049f22ff2e
Mendelt Siebenga <msiebenga@gmail.com>
2020-08-31 23:35:51 +0200

    insert json, does not work

--- examples/json.rs
+++ examples/json.rs
@@ -1,13 +1,22 @@
+use async_trait::async_trait;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::*;
-use sqlx::{query_as, PgPool, types::Json};
-use async_trait::async_trait;
+use sqlx::{postgres::PgDone, query, query_as, types::Json, PgPool};
 
 const DB_URI: &str = "postgresql://example-user:password123@localhost:5432/example-database";
 
 #[async_std::main]
 async fn main() -> Result<(), sqlx::Error> {
     let pool = connect_db(DB_URI).await;
+
+    let blob = Blob {
+        field: "string".to_string(),
+        intfield: 5,
+    };
+
+    pool.store_jsonstuff("stuff".to_string(), Json(blob))
+        .await
+        .unwrap();
 
     let stuff = pool.list_jsonstuff().await?;
 
@@ -19,6 +28,18 @@ async fn main() -> Result<(), sqlx::Error> {
 #[async_trait]
 pub trait JsonTableRepo {
     fn get_database(&self) -> &PgPool;
+
+    async fn store_jsonstuff(
+        &self,
+        name: String,
+        json_stuff: Json<Blob>,
+    ) -> Result<PgDone, sqlx::Error> {
+        query("INSERT INTO json_table (name, json_stuff) VALUES (?, ?);")
+            .bind(name)
+            .bind(json_stuff)
+            .execute(self.get_database())
+            .await
+    }
 
     async fn list_jsonstuff(&self) -> Result<Vec<JsonStuff>, sqlx::Error> {
         query_as::<_, JsonStuff>("SELECT id, name, json_stuff FROM json_table")
@@ -46,7 +67,6 @@ async fn connect_db(uri: &str) -> PgPool {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Blob {
-    field1: String,
-    field2: String,
+    field: String,
     intfield: i64,
 }
