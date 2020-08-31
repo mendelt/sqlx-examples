39c2cbac13580a5fdd7cafa5532231c9dbdf5e25
Mendelt Siebenga <msiebenga@gmail.com>
2020-08-31 23:10:18 +0200

    map directly to serializable

--- examples/json.rs
+++ examples/json.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
 use sqlx::prelude::*;
-use sqlx::{query_as, PgPool};
+use sqlx::{query_as, PgPool, types::Json};
+use async_trait::async_trait;
 
 const DB_URI: &str = "postgresql://example-user:password123@localhost:5432/example-database";
 
@@ -9,30 +9,44 @@ const DB_URI: &str = "postgresql://example-user:password123@localhost:5432/examp
 async fn main() -> Result<(), sqlx::Error> {
     let pool = connect_db(DB_URI).await;
 
-    let stuff = pool.list_jsonstuff().await;
+    let stuff = pool.list_jsonstuff().await?;
 
     println!("{:?}", stuff);
 
     Ok(())
 }
 
+#[async_trait]
 pub trait JsonTableRepo {
     fn get_database(&self) -> &PgPool;
 
     async fn list_jsonstuff(&self) -> Result<Vec<JsonStuff>, sqlx::Error> {
         query_as::<_, JsonStuff>("SELECT id, name, json_stuff FROM json_table")
-            .fetch_all(self.get_database)
-            .await?;
+            .fetch_all(self.get_database())
+            .await
+    }
+}
+
+impl JsonTableRepo for PgPool {
+    fn get_database(&self) -> &PgPool {
+        &self
     }
 }
 
 #[derive(Debug, FromRow)]
-struct JsonStuff {
+pub struct JsonStuff {
     id: i64,
     name: String,
-    json_stuff: Value,
+    json_stuff: Json<Blob>,
 }
 
 async fn connect_db(uri: &str) -> PgPool {
     PgPool::connect(uri).await.unwrap()
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Blob {
+    field1: String,
+    field2: String,
+    intfield: i64,
 }
